bae1877339dbfa25be1abdd7f8c23f70b34565bb
Ankit Verma <85746140+iankitverma@users.noreply.github.com>
2024-05-22 20:54:20 +0530

    feat: added login section

--- components/header.tsx
+++ components/header.tsx
@@ -1,121 +1,129 @@
-'use client';
+"use client";
 
-import React, { Dispatch, SetStateAction } from 'react';
-import { Bars3Icon } from '@heroicons/react/24/outline';
-import { Fragment } from 'react';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { signOut, useSession } from 'next-auth/react';
+import React, { Dispatch, SetStateAction } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
-import Loading from './loading';
-import { useRouter } from 'next/navigation';
-import { classNames } from '@/lib/utils';
+import Loading from "./loading";
+import { useRouter } from "next/navigation";
+import { classNames } from "@/lib/utils";
+import { Button } from "./ui/button";
 interface HeaderProps {
   setSidebarOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 const Header = ({ setSidebarOpen }: HeaderProps) => {
-    const { data: session, status } = useSession();
-    const router = useRouter();
-    // if (status === 'loading') {
-    //   return <Loading /> 
-    // }
-    
-    return (
-      <>
-        <Disclosure as="nav" className="bg-gray-900 sticky top-0 z-40">
-          {() => (
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-              <div className="relative flex h-16 items-center justify-between">
-                <div className="absolute inset-y-0 left-0 flex items-center xl:hidden">
-                  <div className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                    <button
-                      type="button"
-                      className="-m-2.5 p-2.5 text-white block xl:hidden"
-                      onClick={() => setSidebarOpen(true)}
-                    >
-                      <span className="sr-only">Open sidebar</span>
-                      <Bars3Icon className="h-5 w-5" aria-hidden="true" />
-                    </button>
-                  </div>
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  return (
+    <>
+      <Disclosure as="nav" className="bg-gray-900 sticky top-0 z-40">
+        {() => (
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center xl:hidden">
+                <div className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <button
+                    type="button"
+                    className="-m-2.5 p-2.5 text-white block xl:hidden"
+                    onClick={() => setSidebarOpen(true)}
+                  >
+                    <span className="sr-only">Open sidebar</span>
+                    <Bars3Icon className="h-5 w-5" aria-hidden="true" />
+                  </button>
                 </div>
-                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"></div>
-                {session?.user.id && (
-                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <Menu as="div" className="relative ml-3">
-                      <div>
-                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                          <span className="absolute -inset-1.5" />
-                          <span className="sr-only">Open user menu</span>
-                          <img
-                            className="h-8 w-8 bg-gray-300 rounded-full"
-                            src={session?.user?.image ?? ''}
-                            alt=""
-                          />
-                        </Menu.Button>
-                      </div>
-                      <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                      >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="/my-debates"
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
-                              >
-                                My Debates
-                              </a>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <a
-                                href="/categories"
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
-                              >
-                                Categories
-                              </a>
-                            )}
-                          </Menu.Item>
-                          <Menu.Item>
-                            {({ active }) => (
-                              <button
-                                className={classNames(
-                                  active ? 'bg-gray-100' : '',
-                                  'block px-4 py-2 text-sm text-gray-700'
-                                )}
-                                onClick={() => {
-                                  signOut({ redirect: false }).then(() =>
-                                    router.push('/')
-                                  );
-                                }}
-                              >
-                                Sign out
-                              </button>
-                            )}
-                          </Menu.Item>
-                        </Menu.Items>
-                      </Transition>
-                    </Menu>
-                  </div>
-                )}
               </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"></div>
+              {session?.user.id && (
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="absolute -inset-1.5" />
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 bg-gray-300 rounded-full"
+                          src={session?.user?.image ?? ""}
+                          alt={session?.user?.name!}
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/my-debates"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              My Debates
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="/categories"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                            >
+                              Categories
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}
+                              onClick={() => {
+                                signOut({ redirect: false }).then(() =>
+                                  router.push("/")
+                                );
+                              }}
+                            >
+                              Sign out
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              )}
+              {!session && (
+                <button
+                className="bg-white text-black px-3.5 py-1.5 rounded-md hover:bg-gray-200 font-medium mr-3 lg:mr-0"
+                  onClick={async () => {
+                    await signIn();
+                  }}
+                >
+                  Login
+                </button>
+              )}
             </div>
-          )}
-        </Disclosure>
-      </>
-    );
+          </div>
+        )}
+      </Disclosure>
+    </>
+  );
 };
 export default Header;
