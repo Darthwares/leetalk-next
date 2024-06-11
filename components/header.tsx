3ca264eb2f291256abc2c9af9c949535cc875c38
Ankit Verma <85746140+iankitverma@users.noreply.github.com>
2024-06-11 20:47:15 +0530

    feat: added bg white to header

--- components/header.tsx
+++ components/header.tsx
@@ -57,7 +57,7 @@ const Header = ({ setSidebarOpen }: HeaderProps) => {
   ];
 
   return (
-    <Disclosure as="nav" className="sticky top-0 z-40">
+    <Disclosure as="nav" className="sticky top-0 z-40 bg-white">
       {() => (
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
